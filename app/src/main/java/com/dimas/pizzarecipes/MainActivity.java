dc13cd0ff7aa4bcd2724433517c17c783dcb901a
Dimas <dvserhienko.pb@gmail.com>
2022-11-22 21:44:54 +0200

    update

--- app/src/main/java/com/dimas/pizzarecipes/MainActivity.java
+++ app/src/main/java/com/dimas/pizzarecipes/MainActivity.java
@@ -1,6 +1,7 @@
 package com.dimas.pizzarecipes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -17,12 +18,15 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
-    private RecyclerView.LayoutManager layoutManager;
+    private GridLayoutManager layoutManager;
+    private int columnCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        columnCount = getResources().getInteger(R.integer.column_count);
 
         ArrayList<RecycleViewItem> recycleViewItem = new ArrayList<>();
         recycleViewItem.add(new RecycleViewItem(R.drawable.b1, "Волынский борщ", "При подаче готовое блюдо украсьте свежей зеленью."));
@@ -30,12 +34,10 @@ public class MainActivity extends AppCompatActivity {
         recycleViewItem.add(new RecycleViewItem(R.drawable.b3, "Донецкий борщ", "Традиционно такой борщ особенно хорош с черным хлебом, а не с привычными пампушками с чесноком, именно это и отличает его от других вариаций украинских борщей."));
         recycleViewItem.add(new RecycleViewItem(R.drawable.b4, "Зеленый борщ по-украински", "При подаче на стол щедро посыпьте порцию мелко шинкованной зеленью, зеленым луком, добавьте в тарелку половинку сваренного вкрутую яйца. Приятного аппетита!"));
         recycleViewItem.add(new RecycleViewItem(R.drawable.b5, "Борщ киевский", "Отдельно поджарьте на сале сосиски, предварительно нарезанные кружочками. Добавьте их в готовый борщ после того, как выключите его."));
-
-
         recyclerView = findViewById(R.id.recycleView);
         recyclerView.setHasFixedSize(true);
         adapter = new RecycleViewAdapter(recycleViewItem, this);
-        layoutManager = new LinearLayoutManager(this);
+        layoutManager = new GridLayoutManager(this, columnCount);
 
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(layoutManager);
--- app/src/main/java/com/dimas/pizzarecipes/RecycleViewAdapter.java
+++ app/src/main/java/com/dimas/pizzarecipes/RecycleViewAdapter.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 
 public class RecycleViewAdapter extends RecyclerView.Adapter<RecycleViewAdapter.RecyclerViewViewHolder> {
 
-    private ArrayList <RecycleViewItem> arrayList;
+    private final ArrayList <RecycleViewItem> arrayList;
     Context context;
 
     public class RecyclerViewViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
@@ -21,28 +21,28 @@ public class RecycleViewAdapter extends RecyclerView.Adapter<RecycleViewAdapter.
         public TextView text1;
         public TextView text2;
 
-        public RecyclerViewViewHolder (@NonNull View itemView){
+        public RecyclerViewViewHolder (@NonNull final View itemView){
             super(itemView);
             itemView.setOnClickListener(this);
-            imView = itemView.findViewById(R.id.imView);
-            text1 = itemView.findViewById(R.id.tvHead);
-            text2 = itemView.findViewById(R.id.tvBody);
+            this.imView = itemView.findViewById(R.id.imView);
+            this.text1 = itemView.findViewById(R.id.tvHead);
+            this.text2 = itemView.findViewById(R.id.tvBody);
         }
 
         @Override
-        public void onClick(View view) {
-            int position = getAdapterPosition();
-            RecycleViewItem recycleViewItem = arrayList.get(position);
+        public void onClick(final View view) {
+            final int position = this.getAdapterPosition();
+            final RecycleViewItem recycleViewItem = RecycleViewAdapter.this.arrayList.get(position);
 
-            Intent intent = new Intent(context, RecipeActivity.class);
+            final Intent intent = new Intent(RecycleViewAdapter.this.context, RecipeActivity.class);
             intent.putExtra("imRes", recycleViewItem.getImageRes());
             intent.putExtra("tvTitle", recycleViewItem.getText1());
             intent.putExtra("tvDesc", recycleViewItem.getText2());
-            context.startActivity(intent);
+            RecycleViewAdapter.this.context.startActivity(intent);
         }
     }
 
-    public RecycleViewAdapter(ArrayList<RecycleViewItem> arrayList, Context context) {
+    public RecycleViewAdapter(final ArrayList<RecycleViewItem> arrayList, final Context context) {
         this.arrayList = arrayList;
         this.context = context;
 
@@ -50,16 +50,16 @@ public class RecycleViewAdapter extends RecyclerView.Adapter<RecycleViewAdapter.
 
     @NonNull
     @Override
-    public RecyclerViewViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)  {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.res_view_item,
+    public RecyclerViewViewHolder onCreateViewHolder(@NonNull final ViewGroup parent, final int viewType)  {
+        final View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.res_view_item,
                 parent, false);
-        RecyclerViewViewHolder recyclerViewViewHolder = new RecyclerViewViewHolder(view);
+        final RecyclerViewViewHolder recyclerViewViewHolder = new RecyclerViewViewHolder(view);
         return recyclerViewViewHolder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerViewViewHolder holder, int position) {
-        RecycleViewItem recycleViewItem = arrayList.get(position);
+    public void onBindViewHolder(@NonNull final RecyclerViewViewHolder holder, final int position) {
+        final RecycleViewItem recycleViewItem = this.arrayList.get(position);
 
         holder.imView.setImageResource(recycleViewItem.getImageRes());
         holder.text1.setText(recycleViewItem.getText1());
@@ -68,6 +68,6 @@ public class RecycleViewAdapter extends RecyclerView.Adapter<RecycleViewAdapter.
 
     @Override
     public int getItemCount() {
-        return arrayList.size();
+        return this.arrayList.size();
     }
 }
